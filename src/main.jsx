2db3ed34bae05b6b6f23eccd517f4c164060228f
Raph <raphael@betterboards.net>
2026-02-12 10:09:19 +1100

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Plausible from '@plausible-analytics/tracker'
 import App from './App'
 import './index.css'
+
+Plausible({ domain: 'blurrr.it' }).enableAutoPageviews()
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
